696d10875cd21df2b146408b8eaf51bf3a545468
Lars-Kristian Dahl <lars.dahl@gmail.com>
2019-08-03 23:13:22 +0200

    Add documentation

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 import * as functions from 'firebase-functions';
 import {https} from 'firebase-functions';
 import * as admin from "firebase-admin";
-import {HttpsError} from "firebase-functions/lib/providers/https";
+import {CallableContext, HttpsError} from "firebase-functions/lib/providers/https";
 import FieldValue = admin.firestore.FieldValue;
 
 enum HangerState {
@@ -19,6 +19,11 @@ const runtimeOpts = {
 
 const db = admin.firestore(admin.initializeApp());
 
+/**
+ * Finds an available hanger, if any.
+ * Returns an available hanger, or null if no hanger was found.
+ * @param sectionRef The wardrobe section to search through.
+ */
 async function findAvailableHanger(sectionRef: FirebaseFirestore.DocumentReference) {
     const hangers = await db.collection(sectionRef.path + `/hangers`)
         .where('state', "==", HangerState.AVAILABLE)
@@ -28,29 +33,45 @@ async function findAvailableHanger(sectionRef: FirebaseFirestore.DocumentReferen
 }
 
 
+/**
+ * Reserves a hanger.
+ * @param ref Reference to the hanger that should be reserved.
+ */
 function reserveHanger(ref: FirebaseFirestore.DocumentReference) {
     const data = {'state': HangerState.TAKEN, 'stateUpdated': FieldValue.serverTimestamp()};
     return ref.update(data)
 }
 
+
+/**
+ * Requests a check-out
+ */
+// noinspection JSUnusedGlobalSymbols
 export const requestCheckOut = onCall(async (data, context) => {
 
 });
 
+/**
+ * Convenience function to create a https call function.
+ * @param handler The call handler
+ */
 function onCall(handler: (data: any, context: https.CallableContext) => any) {
     return functions.region('europe-west2').https.onCall(handler);
+}
 
+/**
+ * Gets the firebase UID of the user who invoked the function.
+ * @param context
+ */
+function getUser(context: CallableContext) {
+    // context.auth is undefined when running in the emulator, provide a default uid
+    return context.auth === undefined ? 'UyasY6VeR4OY3R4Z3r2xFy9cASh2' : context.auth.uid;
 }
 
 async function createReservation(hanger: FirebaseFirestore.QueryDocumentSnapshot, venueRef: FirebaseFirestore.DocumentReference, context: functions.https.CallableContext, sectionRef: FirebaseFirestore.DocumentReference, wardrobeRef: FirebaseFirestore.DocumentReference) {
     const hangerName: string = await hanger.get('id');
     const venueName = (await venueRef.get()).get('name');
-
-    // if (context.auth === undefined) throw new HttpsError("unauthenticated", "");
-    const userId = context.auth === undefined ? 'UyasY6VeR4OY3R4Z3r2xFy9cASh2' : context.auth.uid;
-    // const userId =
-
-
+    const userId = getUser(context);
     const userRef = db.doc(`users/${userId}`);
 
     const reservationData = {
@@ -68,6 +89,12 @@ async function createReservation(hanger: FirebaseFirestore.QueryDocumentSnapshot
     return {reservation: ref};
 }
 
+/**
+ * Requests a check-in.
+ * Finds and available hanger, reserves it and creates a new reservation entry.
+ *
+ * Returns 'resource-exhausted' if there are no available hangers.
+ */
 // noinspection JSUnusedGlobalSymbols
 export const requestCheckIn = onCall(async (data, context) => {
     const code = tokenize(data.code);
@@ -88,5 +115,6 @@ class QrCode {
 }
 
 function tokenize(code: string) {
+    // TODO: return the real values
     return new QrCode("aaXt3hxtb5tf8aTz1BNp", "E8blVz5KBFZoLOTLJGf1", "vnEpTisjoygX3UJFaMy2");
 }
