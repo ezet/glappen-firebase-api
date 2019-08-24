bffc509a1b5dec467ed8e42ef8cd870942e0a512
Lars-Kristian Dahl <lars.dahl@gmail.com>
2019-08-25 01:56:15 +0200

    Update confirmCheckIn
    Fix requestCheckOut

--- functions/src/handlers/confirmCheckInHandler.ts
+++ functions/src/handlers/confirmCheckInHandler.ts
@@ -1,18 +1,23 @@
 import * as functions from "firebase-functions";
-import {adminVisibilityForState, clientVisibilityForState, db, ReservationState} from "../utils";
+import {adminVisibilityForState, clientVisibilityForState, db, ReservationState, stripe} from "../utils";
 import {FieldValue} from "@google-cloud/firestore";
 
 export async function confirmCheckInHandler(data: any, context: functions.https.CallableContext) {
-    const reservation: string = data.reservationId;
-    const ref = db.doc(`reservations/${reservation}`);
+    const reservationRef = db.doc(`reservations/${data.reservationId}`);
+    const reservation = await reservationRef.get();
+    const paymentIntentId = reservation.get('paymentIntent');
+
+    await stripe.paymentIntents.capture(paymentIntentId);
+
     const newState = ReservationState.CHECKED_IN;
     const payload = {
         checkedIn: FieldValue.serverTimestamp(),
         stateUpdated: FieldValue.serverTimestamp(),
         state: newState,
+        eligibleForTimeout: false,
         visibleInApp: clientVisibilityForState(newState),
         visibleInAdmin: adminVisibilityForState(newState)
     };
-    const wr = await ref.update(payload);
-    return {writeTime: wr.writeTime};
+    await reservationRef.update(payload);
+    return {}
 }
--- functions/src/handlers/requestCheckOutHandler.ts
+++ functions/src/handlers/requestCheckOutHandler.ts
@@ -1,15 +1,17 @@
 import * as functions from "firebase-functions";
-import {db, ReservationState} from "../utils";
+import {adminVisibilityForState, clientVisibilityForState, db, ReservationState} from "../utils";
 import {FieldValue} from "@google-cloud/firestore";
 
 // noinspection JSUnusedLocalSymbols
 export async function requestCheckOutHandler(data: any, context: functions.https.CallableContext) {
-    const reservation: string = data.reservation;
-    const ref = db.doc(`reservations/${reservation}`);
+    const ref = db.doc(`reservations/${data.reservationId}`);
+
+    const newState = ReservationState.CHECKING_OUT;
     const newData = {
-        state: ReservationState.CHECKING_OUT,
+        state: newState,
+        visibleInApp: clientVisibilityForState(newState),
+        visibleInAdmin: adminVisibilityForState(newState),
         stateUpdated: FieldValue.serverTimestamp()
     };
-    const wr = await ref.update(newData);
-    return {writeTime: wr.writeTime};
+    return ref.update(newData);
 }
